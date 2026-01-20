e0cc1445ec0e4f9601741dd830b2ee22e2860bc3
IT-User <stepan.gorshkov.05@mail.ru>
2026-01-20 14:17:46 +0700

    20.01.2026

--- app/src/main/java/com/example/customtimer/MainActivity.kt
+++ app/src/main/java/com/example/customtimer/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.customtimer
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
@@ -14,10 +16,13 @@ import kotlin.time.DurationUnit
 
 class MainActivity : ComponentActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mediaPlayer = MediaPlayer()
 
         binding.btStart.setOnClickListener {
             timerStart(binding.edHour.text.toString(),binding.edMin.text.toString(),binding.edSec.text.toString())
@@ -27,6 +32,10 @@ class MainActivity : ComponentActivity() {
         val sum = ((hour.toInt().hours)+(minute.toInt().minutes)+(second.toInt().seconds)).toLong(DurationUnit.MILLISECONDS)
         object : CountDownTimer(sum,1000){
             override fun onFinish() {
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+                mediaPlayer.setDataSource(R.layout.activity_main)
+                mediaPlayer.prepare()
+                mediaPlayer.start()
                 binding.tvText.setText("Готово")
             }
 
