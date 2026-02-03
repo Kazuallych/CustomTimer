32550e941b0ba890432f0bfeeab6638ceb5df376
Stepa <stepan.gorshkov.05@mail.ru>
2026-02-03 11:54:01 +0700

    03.02 11:53

--- app/src/main/java/com/example/customtimer/MainActivity.kt
+++ app/src/main/java/com/example/customtimer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,6 +22,7 @@ class MainActivity : ComponentActivity() {
     lateinit var adapterTimer: AdapterTimer
     lateinit var mediaPlayer: MediaPlayer
     var positionTimer = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,12 +30,18 @@ class MainActivity : ComponentActivity() {
 
         data = ArrayList()
         dataTimer = ArrayList()
+        var tempTimers = ArrayList<Item>()
         val arrayTimer = ArrayList<CountDownTimer>()
-        data.add(Item(10.seconds))
+        var isRunning = false
+        data.add(Item(10.1.seconds))
 
         //Запуск последовательности таймеров
         binding.btStart.setOnClickListener {
+            isRunning = true
             if(dataTimer.isNotEmpty()) {
+                tempTimers.clear()
+                tempTimers = dataTimer.map { it.copy() }.toCollection(ArrayList())
+                visibleGone()
                 positionTimer = 0
                 arrayTimer[positionTimer].start()
             }else{
@@ -45,10 +53,31 @@ class MainActivity : ComponentActivity() {
             if(dataTimer.isNotEmpty()){
                 adapterTimer.notifyItemRangeRemoved(0,dataTimer.size)
                 dataTimer.clear()
+                arrayTimer.clear()
                 Toast.makeText(this,"Список полностью очищен",Toast.LENGTH_SHORT).show()
             }else{
                 Toast.makeText(this,"Список уже пуст",Toast.LENGTH_SHORT).show()
             }
+        }
+        //Полная остановка всех таймеров после их запуска
+        binding.btFullStop.setOnClickListener {
+            for(i in 0..arrayTimer.size-1){
+                arrayTimer[0].cancel()
+            }
+            refreshItems(tempTimers)
+            visibleShow()
+        }
+        //Остановка и запуск уже запущенной последовательности таймеров
+        binding.btStop.setOnClickListener{
+            val tempTime = dataTimer[positionTimer].time
+            if(isRunning){
+                isRunning = false
+                arrayTimer[positionTimer].cancel()
+            }else{
+                arrayTimer[positionTimer].start()
+                isRunning = true
+            }
+
         }
         //Определение музыки
         mediaPlayer = MediaPlayer()
@@ -67,14 +96,13 @@ class MainActivity : ComponentActivity() {
                 mediaPlayer.stop()
             }
         },{position->
-            dataTimer.add(data[position])
-
-            val tempTimers = dataTimer.map { it.copy() }.toCollection(ArrayList())
+            dataTimer.add(data[position].copy())
 
             val timerAdd = object : CountDownTimer((dataTimer[positionTimer].time.toLong(DurationUnit.MILLISECONDS)),1000){
                 override fun onTick(millisUntilFinished: Long) {
                     adapterTimer.notifyItemChanged(positionTimer,"TEXT_CHANGED")
                     dataTimer[positionTimer].time = millisUntilFinished.milliseconds
+
                 }
                 override fun onFinish() {
                     positionTimer++
@@ -82,14 +110,9 @@ class MainActivity : ComponentActivity() {
                         arrayTimer[positionTimer].start()
                     }else{
                         //Обновление элементов после завершения таймеров чтобы вернулся изначальный текст
-                        dataTimer.clear()
-                        dataTimer.addAll(tempTimers.map{it.copy()})
-                        for(i in 0..dataTimer.size-1){
-                            adapterTimer.notifyItemChanged(i,"TEXT_CHANGED")
-                        }
-                        tempTimers.clear()
+                        refreshItems(tempTimers)
                     }
-                }
+                }:
             }
             arrayTimer.add(timerAdd)
             adapterTimer.notifyItemInserted(dataTimer.size-1)
@@ -97,5 +120,24 @@ class MainActivity : ComponentActivity() {
         binding.rcView.layoutManager = LinearLayoutManager(this)
         binding.rcView.adapter = adapter
 
+    }
+    fun refreshItems(tempTimers: ArrayList<Item>){
+        dataTimer.clear()
+        dataTimer.addAll(tempTimers.map{it.copy()})
+        for(i in 0..dataTimer.size-1){
+            adapterTimer.notifyItemChanged(i,"TEXT_CHANGED")
+        }
+    }
+    fun visibleGone(){
+        binding.btStart.visibility = View.GONE
+        binding.btClear.visibility = View.GONE
+        binding.btFullStop.visibility = View.VISIBLE
+        binding.btStop.visibility = View.VISIBLE
+    }
+    fun visibleShow(){
+        binding.btStart.visibility = View.VISIBLE
+        binding.btClear.visibility = View.VISIBLE
+        binding.btStop.visibility = View.GONE
+        binding.btFullStop.visibility = View.GONE
     }
 }
